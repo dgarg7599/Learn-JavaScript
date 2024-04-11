03fd30dd288a39f6518a6e0dc3a1a18df29347e6
Divyansh Garg <142008285+dgarg7599@users.noreply.github.com>
2024-04-11 09:42:32 +0000

    DataTypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,78 @@
+//* Data types define the type of values that a variable can hold.
+
+"use strict"; //treat all JS code as newer versions.
+
+//alert(3+3)  //not working here because we are using nodejs, not browser.
+
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Divyansh")
+
+
+let name = "divyansh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+/*
+
+Data Types ==> 1. Primitive ==> 
+
+                    // number => 2 to power 53
+                    // bigint
+                    // string => ""
+                    // boolean => true/false
+                    // null => standalone value
+                    // undefined => 
+                    // symbol => unique
+
+               2. Object ==> Object, Array, Date
+
+
+
+//? Types of Primitive Data types
+
+//? Number: Represents numeric values, including integers and floating-point numbers.
+// Example:
+// var myFavNum = -5;
+// console.log(myFavNum);
+
+//? String: Represents a sequence of characters enclosed in single or double quotes.
+// Example:
+// var myName = 'vinod';
+// console.log(myName);
+
+//? Boolean: Represents a logical entity with two values: true or false.
+// Example:
+// var isRaining = false;
+// var areYouAwesome = true;
+// console.log(isRaining);
+
+//? undefined: Represents the absence of a value or an uninitialized variable.
+// Example:
+// var vinod;
+// console.log(vinod);
+
+//? Null: Represents the absence of a value. It is often used to explicitly indicate that a variable or object property has no assigned value.
+// Example:
+// var badMemories = null;
+// console.log(badMemories);
+
+//? BigInt: Represents integers of arbitrary precision (available since ECMAScript 2020).
+// Example:
+// const bigNumber = 1234567890123456789012345678901234567890n;
+
+//? Symbol: Represents a unique and immutable data type, often used to create unique identifiers.
+// Example:
+// const mySymbol = Symbol("description");
+
+
+
+*/

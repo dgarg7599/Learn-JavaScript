cb6bf32b44c3b7839b65ed6f6d84b51571fabc97
Divyansh Garg <142008285+dgarg7599@users.noreply.github.com>
2024-04-11 06:44:29 +0000

    learning Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+
+//  ** Variables: A variable is a container that holds a value. It has a name and can be used to store and manipulate data in a program.
+
+// ** Values:  A value is a piece of information that a program can work with. It can be a number, text, true/false, or more complex data.
+
+
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"    //not to use like this but Js allows it.
+let accountState;     //undefined
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope. Js changes var to let.
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+//it can print in tabular form

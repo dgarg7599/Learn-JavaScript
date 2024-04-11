218fc01883ae56af761f1ccc8c18e0f10df6a0b4
Divyansh Garg <142008285+dgarg7599@users.noreply.github.com>
2024-04-11 10:00:18 +0000

    Conversion Operation and Concatenation done

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,105 @@
+let score =  "divyansh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn =  "divyansh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//  "divyansh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " divyansh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+/*
+
+//* Concatenation in JavaScript
+//? In JavaScript, the + sign is not only used for arithmetic addition but also for string concatenation. When the + operator is used with strings, it concatenates the strings together.
+//? It's important to note that if any operand of the + operator is a string, JavaScript will treat the other operands as strings as well, resulting in string concatenation. If both operands are numbers, the + operator performs numeric addition.
+
+// const str = "Hello " + "World";
+// console.log(str);
+
+//* Type coercion is the automatic conversion of "values" from one data type to another.
+//? It is a fundamental part of JavaScript and can be used to make code more readable and efficient.
+//? There are two types of coercion in JavaScript: implicit and explicit. Implicit coercion happens automatically, while explicit coercion is done manually by the programmer.
+//! It's worth noting that type coercion can lead to unexpected results, so it's essential to be aware of how JavaScript handles these situations.
+
+// let sum = "5" + 10;
+// console.log(sum);
+
+
+// ----------- Some Tricky Interview Questions --------------------------
+
+// console.log(10 + "20");
+// console.log(9 - "5");
+// console.log("Java" + "Script");
+// console.log(" " + " ");
+// let sum = " " + 0;
+// console.log(typeof sum);
+// console.log("vinod" - "thapa");
+// console.log(true + true);
+// console.log(true + false);
+// console.log(false + true);
+// console.log(false - true);
+
+
+
+*/

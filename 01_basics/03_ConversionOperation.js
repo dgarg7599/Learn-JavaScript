74a0288cb2382275857793025018d4e1db897996
dgarg7599 <gargdivyansh7599@gmail.com>
2024-04-14 15:31:44 +0530

    Conversion Operators done

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -68,15 +68,6 @@ console.log(gameCounter);
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
 
-/*
-
-//* Concatenation in JavaScript
-//? In JavaScript, the + sign is not only used for arithmetic addition but also for string concatenation. When the + operator is used with strings, it concatenates the strings together.
-//? It's important to note that if any operand of the + operator is a string, JavaScript will treat the other operands as strings as well, resulting in string concatenation. If both operands are numbers, the + operator performs numeric addition.
-
-// const str = "Hello " + "World";
-// console.log(str);
-
 
 
 // ------------------------ Most Imp. --------------------------
@@ -106,7 +97,4 @@ console.log(gameCounter);
 // console.log(false + true);
 // console.log(false - true);
 
-...
-
-*/
-
+// 

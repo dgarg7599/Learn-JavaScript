ba24139b87934f23cefd43540611b48bbfeef51a
dgarg7599 <gargdivyansh7599@gmail.com>
2024-04-15 14:42:24 +0530

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name = "divyansh"
+const repoCount = 50
+
+console.log(name + repoCount + "Value");
+
+
+//* Concatenation in JavaScript
+//? In JavaScript, the + sign is not only used for arithmetic addition but also for string concatenation. When the + operator is used with strings, it concatenates the strings together.
+//? It's important to note that if any operand of the + operator is a string, JavaScript will treat the other operands as strings as well, resulting in string concatenation. If both operands are numbers, the + operator performs numeric addition.
+
+// const str = "Hello " + "World";
+// console.log(str);

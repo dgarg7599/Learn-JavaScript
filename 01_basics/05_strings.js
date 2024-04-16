72e364775bf29f2c5beb47eb1670080fcd617d3b
dgarg7599 <gargdivyansh7599@gmail.com>
2024-04-16 13:04:24 +0530

    Strings basics done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,7 +1,14 @@
+//? Strings in JavaScript are a fundamental data type that represents a sequence of characters.
+
+// Note:
+//? Strings created with single or double quotes works the same.
+// There is no difference between the two.
+
+
 const name = "divyansh"
 const repoCount = 50
 
-console.log(name + repoCount + "Value");
+// console.log(name + repoCount + "Value");
 
 
 //* Concatenation in JavaScript
@@ -10,3 +17,37 @@ console.log(name + repoCount + "Value");
 
 // const str = "Hello " + "World";
 // console.log(str);
+
+console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`);                                      //same as concatenate the string.
+
+
+
+
+const gameName = new String('divyansh-dg-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   divyansh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://divyansh.com/divyansh%20garg"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
